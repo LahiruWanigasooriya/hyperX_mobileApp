aacb79adedcdcbf7e4a17892eb7be629189b6f81
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2023-09-20 22:17:01 +0530

    basic ui

---
+++ Assets/scripts/Gamemaneger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Gamemaneger : MonoBehaviour
+{
+    [SerializeField] private TMP_Text welcomText;
+    [SerializeField] private TMP_InputField LoginnameInpuFeald,LoginPassword;
+    [SerializeField] private TMP_InputField signeUpemailInput;
+    [SerializeField] private TMP_InputField emailInput;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
